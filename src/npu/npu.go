233a27c9442774b02857b842e275a953bf72c644
criticalsession <criticalsession@protonmail.ch>
2023-12-26 08:07:37 +0100

    Added directory upload functionality

--- src/npu/npu.go
+++ src/npu/npu.go
@@ -2,25 +2,34 @@ package main
 
 import (
 	"bytes"
+	"errors"
 	"flag"
 	"fmt"
 	"io"
-	"io/ioutil"
 	"mime/multipart"
 	"net/http"
 	"os"
 	"path/filepath"
 	"strconv"
+	"strings"
 	"time"
 
+	"github.com/rodaine/table"
 	"golang.design/x/clipboard"
 )
+
+type uploadedFile struct {
+	fileName string
+	upUrl    string
+}
 
 func main() {
 	expires := flag.Int("expires", 0, "set expiration time for the uploaded file in hours")
 	secret := flag.Bool("secret", false, "enable secret mode")
+	dirMode := flag.Bool("dir", false, "enable directory mode (non-recursive)")
+
 	flag.Usage = func() {
-		fmt.Fprintf(os.Stderr, "Usage: [-expires <hours>] [-secret] <filePath>\n")
+		fmt.Fprintf(os.Stderr, "Usage: [-expires <hours>] [-secret] [-dir] <directory/filePath>\n")
 		flag.PrintDefaults()
 		fmt.Print("\n")
 		os.Exit(1)
@@ -31,12 +40,100 @@ func main() {
 	if len(args) < 1 {
 		flag.Usage()
 	}
-	filePath := args[0]
 
+	// construct list of files to upload
+	filePaths := []string{}
+	dir := "./"
+	if *dirMode {
+		dir = strings.TrimSuffix(strings.TrimSuffix(args[0], "/"), "\\") // trim trailing slashes
+
+		dirEntries, err := os.ReadDir(dir)
+		if err != nil {
+			fmt.Println("Failed to read files in directory:", err)
+			os.Exit(1)
+		}
+
+		for _, entry := range dirEntries {
+			if !entry.IsDir() {
+				filePaths = append(filePaths, fmt.Sprintf("%s/%s", dir, entry.Name()))
+			}
+		}
+	} else {
+		for _, argPath := range args {
+			filePaths = append(filePaths, argPath)
+		}
+	}
+
+	// upload files
+	uploadedFiles := []uploadedFile{}
+	for _, filePath := range filePaths {
+		responseBody, err := uploadFile(filePath, expires, secret)
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+
+		if responseBody != nil {
+			upUrl := string(responseBody)
+			upUrl = strings.TrimSuffix(strings.TrimSuffix(upUrl, "\n"), "\r")
+
+			uploadedFiles = append(uploadedFiles, uploadedFile{
+				fileName: filepath.Base(filePath),
+				upUrl:    upUrl,
+			})
+		}
+	}
+
+	if len(uploadedFiles) == 1 {
+		// original, single file upload
+		upUrl := uploadedFiles[0].upUrl + "\n"
+		fmt.Println(string(upUrl))
+		clipboard.Write(clipboard.FmtText, []byte(upUrl))
+	} else {
+		// for multiple file upload
+		printTblOutput(&uploadedFiles)
+		logFile, err := writeOutputToFile(dir, &uploadedFiles)
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+
+		fmt.Println("\nLog file saved:", logFile)
+	}
+}
+
+func printTblOutput(uploadedFiles *[]uploadedFile) {
+	tbl := table.New("File", "| 0x0 Url")
+	for _, upFile := range *uploadedFiles {
+		tbl.AddRow(upFile.fileName, "| "+upFile.upUrl)
+	}
+	tbl.Print()
+}
+
+func writeOutputToFile(dir string, uploadedFiles *[]uploadedFile) (logFile string, err error) {
+	logFile = fmt.Sprintf("%s/%s.%d.log", dir, "upload", time.Now().Unix())
+	f, err := os.Create(logFile)
+	if err != nil {
+		return "", errors.New(fmt.Sprintf("Failed to save output log file: %v", err))
+	}
+	defer f.Close()
+
+	for _, upFile := range *uploadedFiles {
+		_, err = f.WriteString(fmt.Sprintf("%s: %s\r\n", upFile.fileName, upFile.upUrl))
+		if err != nil {
+			return "", errors.New(fmt.Sprintf("Failed to write to log file: %v", err))
+		}
+	}
+
+	f.Sync()
+
+	return logFile, nil
+}
+
+func uploadFile(filePath string, expires *int, secret *bool) (responseBody []byte, err error) {
 	file, err := os.Open(filePath)
 	if err != nil {
-		fmt.Println("Failed to open file:", err)
-		os.Exit(1)
+		return nil, errors.New(fmt.Sprintf("Failed to open file: %v", err))
 	}
 	defer file.Close()
 
@@ -44,8 +141,7 @@ func main() {
 	writer := multipart.NewWriter(body)
 	part, err := writer.CreateFormFile("file", filepath.Base(filePath))
 	if err != nil {
-		fmt.Println("Failed to create form file:", err)
-		os.Exit(1)
+		return nil, errors.New(fmt.Sprintf("Failed to create form file: %v", err))
 	}
 	io.Copy(part, file)
 
@@ -62,33 +158,26 @@ func main() {
 
 	err = writer.Close()
 	if err != nil {
-		fmt.Println("Failed to close multipart writer:", err)
-		os.Exit(1)
+		return nil, errors.New(fmt.Sprintf("Failed to close multipart writer: %v", err))
 	}
 
 	request, err := http.NewRequest("POST", "https://0x0.st", body)
 	if err != nil {
-		fmt.Println("Failed to create HTTP request:", err)
-		os.Exit(1)
+		return nil, errors.New(fmt.Sprintf("Failed to create HTTP request: %v", err))
 	}
 	request.Header.Set("Content-Type", writer.FormDataContentType())
 
 	client := &http.Client{}
 	response, err := client.Do(request)
 	if err != nil {
-		fmt.Println("Failed to send HTTP request:", err)
-		os.Exit(1)
+		return nil, errors.New(fmt.Sprintf("Failed to send HTTP request: %v", err))
 	}
 	defer response.Body.Close()
 
-	responseBody, err := ioutil.ReadAll(response.Body)
+	responseBody, err = io.ReadAll(response.Body)
 	if err != nil {
-		fmt.Println("Failed to read response body:", err)
-		os.Exit(1)
+		return nil, errors.New(fmt.Sprintf("Failed to read response body: %v", err))
 	}
 
-	if responseBody != nil {
-		fmt.Println(string(responseBody))
-		clipboard.Write(clipboard.FmtText, []byte(string(responseBody)))
-	}
+	return responseBody, nil
 }
